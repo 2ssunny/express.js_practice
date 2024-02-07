a8d9409532990aabbad348743c4719754b141c2e
Juho Lee <eungseoni@gmail.com>
2024-02-07 01:12:14 -0500

    feat:setting communication with SQL server

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const PORT = process.env.port || 8000;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const cors = require("cors");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "ssunny0203",
+  database: "bbs",
 });
 
-module.exports = router;
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  const sqlQuery = "INSERT INTO requested (rowno) VALUES (1)";
+  db.query(sqlQuery, (err, result) => {
+    console.log(err);
+    res.send("success!");
+  });
+});
+
+app.get("/list", (req, res) => {
+  const sqlQuery =
+    "SELECT BOARD_ID, BOARD_TITLE, REGISTER_ID, DATE_FORMAT(REGISTER_DATE, '%Y-%m-%d') AS REGISTER_DATE FROM BOARD;";
+  db.query(sqlQuery, (err, result) => {
+    res.send(result);
+  });
+});
+
+https: app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
