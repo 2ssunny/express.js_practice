1ff634752b423bc6508bad08ef12fc6c55faaf7a
Juho Lee <eungseoni@gmail.com>
2024-02-07 19:09:34 -0500

    feat; rollback index and user form TS to JS

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,24 +1,11 @@
-import express, { Request, Response } from "express";
-import mysql from "mysql";
+const express = require("express");
 const app = express();
+const mysql = require("mysql");
 const PORT = process.env.port || 8000;
 
 const cors = require("cors");
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "ssunny0203",
-  database: "bbs",
-});
-
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   const sqlQuery = "INSERT INTO requested (rowno) VALUES (1)";
   db.query(sqlQuery, (err, result) => {
     console.log(err);
@@ -26,7 +13,7 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
-app.get("/list", (req: Request, res: Response) => {
+app.get("/list", (req, res) => {
   const sqlQuery =
     "SELECT BOARD_ID, BOARD_TITLE, REGISTER_ID, DATE_FORMAT(REGISTER_DATE, '%Y-%m-%d') AS REGISTER_DATE FROM BOARD;";
   db.query(sqlQuery, (err, result) => {
